14323a94b96aef27b89942ad9a0a5ac86ef0775c
Caroline Benn <lineybenn@gmail.com>
2017-12-30 17:24:39 +0000

    Made work on pi with real raspi command

--- index.rb
+++ index.rb
@@ -2,24 +2,12 @@ require 'sinatra'
 # require 'sinatra/cors'
 require 'sinatra/reloader' if development?
 
+set :bind, '0.0.0.0'
+
 # set :allow_origin, "http://localhost:3000"
 
 # On FE query `/start` to get o
 # CHECK HOW TO RETURN JSON <3
-
-# poses = [
-#   'Jazz flute',
-#   'Look left',
-#   'Angry bear',
-#   'Freezing cold',
-#   'Sex fingers',
-#   'Grumpy AF',
-#   'Kiss',
-#   'Waving',
-#   'Douchebag',
-#   'Vogue',
-#   'Lick',
-# ];
 
 
 # get '/start' do
@@ -32,13 +20,6 @@ require 'sinatra/reloader' if development?
 
 #   # Takes photo and saves in a file named Time.now
 #   # %x(raspistill -vf -hf -o #{filename})
-#   # %x(mkdir #{id})
-#   # %x(touch #{id}/#{id}.angry.1.jpg)
-#   # %x(touch #{id}/#{id}.angry.2.jpg)
-#   # %x(touch #{id}/#{id}.angry.3.jpg)
-#   # %x(touch #{id}/#{id}.cheese.1.jpg)
-#   # %x(touch #{id}/#{id}.cheese.2.jpg)
-#   # %x(touch #{id}/#{id}.cheese.3.jpg)
 #   erb :index
 
 #   # Send JSON objects back as a part of its response.
@@ -47,15 +28,20 @@ require 'sinatra/reloader' if development?
 
 get '/shutter/:id/:pose' do
   headers 'Access-Control-Allow-Origin' => '*'
-  filepath = "photos/#{params[:id]}/#{params[:id]}.#{params[:pose]}"
-  puts %x(mkdir photos/#{params[:id]})
+  filepath = "/home/pi/photobooth/f1_raw/#{params[:id]}/#{params[:id]}.#{params[:pose]}"
+  puts %x(mkdir /home/pi/photobooth/f1_raw/#{params[:id]})
 
   # Per pose - in order to get GIF!
-  3.times do |n|  
-     puts %x(touch #{filepath}.#{n + 1}.jpg)
-  end  
+  #3.times do |n|  
+     #puts %x(touch #{filepath}.#{n + 1}.jpg)
+  puts %x(raspistill -bm -md 1 -q 80 -t 1400 -tl 0 --nopreview -o #{filepath}.%02d.jpg)
+  #end  
   status :ok
   body ''
+end
+
+get '/gallery'
+	body '[ { "id":"12312341414", "data": [{pose: "waving", gif: "blah.gif", pics: [1,2,3,4] }, {}, {}, {}] }, {id} ,{id}  ]'
 end
 
 
